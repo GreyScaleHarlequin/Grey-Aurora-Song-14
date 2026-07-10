b3328b4e315bd013a189dcf8b8d5282b51850535
Falcon <falcon@zigtag.dev>
2026-07-10 15:57:07 -0700

    refactor: directly fetch metadata as upstream intends

--- Content.Shared/_AS/Traits/ReplicantSystem.cs
+++ Content.Shared/_AS/Traits/ReplicantSystem.cs
@@ -22,21 +22,20 @@ public sealed partial class ReplicantSystem : EntitySystem
     private void OnReplicantStartup(EntityUid uid, ReplicantComponent component, ComponentStartup args)
     {
         _typingIndicator.SetTypingIndicator(uid, TypingIndicator);
-        if (TryComp(uid, out MetaDataComponent? metaData)) // Aurora's Song | Grab the MetaData of the player entity so we can see if it's a fairy
+        var metaData = MetaData(uid); // Aurora's Song | Grab the MetaData of the player entity so we can see if it's a fairy
+
+        var prototype = metaData.EntityPrototype;
+        var protoId = prototype?.ID;
+        if (protoId == "MobFairy") // Aurora's Song | For now we're only checking if they're a fairy,
+            // in the future this may become reliant on a tag or component that is used to flag smaller lifeforms, but for now this is fine.
         {
-            var prototype = metaData.EntityPrototype;
-            var protoId = prototype?.ID;
-            if (protoId == "MobFairy") // Aurora's Song | For now we're only checking if they're a fairy,
-                                       // in the future this may become reliant on a tag or component that is used to flag smaller lifeforms, but for now this is fine.
-            {
-                var replicantBlood = new Solution("Oxidant", 120);
-                _bloodSystem.ChangeBloodReagents(uid, replicantBlood); // VDS - update to use new ChangeBloodReagents
-            }
-            else
-            {
-                // Aurora's Song - Moved the normal ChangeBloodReagents call into the selection logic
-                _bloodSystem.ChangeBloodReagents(uid, component.OxidantReagent); // VDS - update to use new ChangeBloodReagents
-            }
+            var replicantBlood = new Solution("Oxidant", 120);
+            _bloodSystem.ChangeBloodReagents(uid, replicantBlood); // VDS - update to use new ChangeBloodReagents
+        }
+        else
+        {
+            // Aurora's Song - Moved the normal ChangeBloodReagents call into the selection logic
+            _bloodSystem.ChangeBloodReagents(uid, component.OxidantReagent); // VDS - update to use new ChangeBloodReagents
         }
 
     }
