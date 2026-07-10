463591af7c36b9be7d4554a46ba64ffd839421b9
Falcon <falcon@zigtag.dev>
2026-07-10 16:55:55 -0700

    refactor: future proofing

--- Content.Shared/_AS/Traits/ReplicantSystem.cs
+++ Content.Shared/_AS/Traits/ReplicantSystem.cs
@@ -1,42 +1,56 @@
+using System.Linq;
+using Content.Shared.Body.Components;
 using Content.Shared.Body.Systems;
 using Content.Shared.Chat.TypingIndicator;
 using Content.Shared.Chemistry.Components;
+using Content.Shared.FixedPoint;
+using Content.Shared.Humanoid.Prototypes;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._AS.Traits;
 
 public sealed partial class ReplicantSystem : EntitySystem
 {
-    private static readonly ProtoId<TypingIndicatorPrototype> TypingIndicator = "robot";
-//    private static readonly ProtoId<ReagentPrototype> Blood = "Oxidant"; // VDS - use solution in component instead.
-
     [Dependency] private SharedBloodstreamSystem _bloodSystem = default!;
     [Dependency] private SharedTypingIndicatorSystem _typingIndicator = default!;
+    [Dependency] private IPrototypeManager _protoMan = default!;
+
+    private static readonly ProtoId<TypingIndicatorPrototype> TypingIndicator = "robot";
+    private readonly Dictionary<EntProtoId, FixedPoint2> _speciesBloodLevels = new();
+
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ReplicantComponent, ComponentStartup>(OnReplicantStartup);
+
+        foreach (var speciesProto in _protoMan.EnumeratePrototypes<SpeciesPrototype>()
+            .Select(proto => _protoMan.Index(proto.Prototype)))
+        {
+            if (!speciesProto.Components.TryGetComponent<BloodstreamComponent>(EntityManager.ComponentFactory,
+                    out var bloodstreamComp))
+                continue;
+
+            var bloodVolume = bloodstreamComp.BloodReferenceSolution.Volume;
+
+            _speciesBloodLevels.Add(speciesProto.ID, bloodVolume);
+        }
     }
 
     private void OnReplicantStartup(EntityUid uid, ReplicantComponent component, ComponentStartup args)
     {
         _typingIndicator.SetTypingIndicator(uid, TypingIndicator);
-        var metaData = MetaData(uid); // Aurora's Song | Grab the MetaData of the player entity so we can see if it's a fairy
 
-        var prototype = metaData.EntityPrototype;
-        var protoId = prototype?.ID;
-        if (protoId == "MobFairy") // Aurora's Song | For now we're only checking if they're a fairy,
-            // in the future this may become reliant on a tag or component that is used to flag smaller lifeforms, but for now this is fine.
-        {
-            var replicantBlood = new Solution("Oxidant", 120);
-            _bloodSystem.ChangeBloodReagents(uid, replicantBlood); // VDS - update to use new ChangeBloodReagents
-        }
-        else
-        {
-            // Aurora's Song - Moved the normal ChangeBloodReagents call into the selection logic
-            _bloodSystem.ChangeBloodReagents(uid, component.OxidantReagent); // VDS - update to use new ChangeBloodReagents
-        }
+        if (!HasComp<BloodstreamComponent>(uid))
+            return;
 
+        var metaData = MetaData(uid);
+
+        var replicantBlood = new Solution(component.OxidantReagent);
+
+        if (metaData.EntityPrototype?.ID is { } protoId && _speciesBloodLevels.TryGetValue(protoId, out var bloodAmount))
+            replicantBlood.ScaleTo(bloodAmount);
+
+        _bloodSystem.ChangeBloodReagents(uid, replicantBlood); // VDS - update to use new ChangeBloodReagents
     }
 }
