5bbaf61065cb7c9ec0e2cef625404f9790fc4f81
Falcon <falcon@zigtag.dev>
2026-07-10 20:30:57 -0700

    refactor: scale to BloodstreamComponent directly

--- Content.Shared/_AS/Traits/ReplicantSystem.cs
+++ Content.Shared/_AS/Traits/ReplicantSystem.cs
@@ -1,10 +1,7 @@
-using System.Linq;
 using Content.Shared.Body.Components;
 using Content.Shared.Body.Systems;
 using Content.Shared.Chat.TypingIndicator;
 using Content.Shared.Chemistry.Components;
-using Content.Shared.FixedPoint;
-using Content.Shared.Humanoid.Prototypes;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared._AS.Traits;
@@ -13,43 +10,26 @@ public sealed partial class ReplicantSystem : EntitySystem
 {
     [Dependency] private SharedBloodstreamSystem _bloodSystem = default!;
     [Dependency] private SharedTypingIndicatorSystem _typingIndicator = default!;
-    [Dependency] private IPrototypeManager _protoMan = default!;
 
     private static readonly ProtoId<TypingIndicatorPrototype> TypingIndicator = "robot";
-    private readonly Dictionary<EntProtoId, FixedPoint2> _speciesBloodLevels = new();
 
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<ReplicantComponent, ComponentStartup>(OnReplicantStartup);
-
-        foreach (var speciesProto in _protoMan.EnumeratePrototypes<SpeciesPrototype>()
-            .Select(proto => _protoMan.Index(proto.Prototype)))
-        {
-            if (!speciesProto.Components.TryGetComponent<BloodstreamComponent>(EntityManager.ComponentFactory,
-                    out var bloodstreamComp))
-                continue;
-
-            var bloodVolume = bloodstreamComp.BloodReferenceSolution.Volume;
-
-            _speciesBloodLevels.Add(speciesProto.ID, bloodVolume);
-        }
     }
 
     private void OnReplicantStartup(EntityUid uid, ReplicantComponent component, ComponentStartup args)
     {
         _typingIndicator.SetTypingIndicator(uid, TypingIndicator);
 
-        if (!HasComp<BloodstreamComponent>(uid))
+        if (!TryComp<BloodstreamComponent>(uid, out var bloodstreamComponent))
             return;
-
-        var metaData = MetaData(uid);
 
         var replicantBlood = new Solution(component.OxidantReagent);
 
-        if (metaData.EntityPrototype?.ID is { } protoId && _speciesBloodLevels.TryGetValue(protoId, out var bloodAmount))
-            replicantBlood.ScaleTo(bloodAmount);
+        replicantBlood.ScaleTo(bloodstreamComponent.BloodReferenceSolution.Volume); // Scale to current bloodstream
 
         _bloodSystem.ChangeBloodReagents(uid, replicantBlood); // VDS - update to use new ChangeBloodReagents
     }
